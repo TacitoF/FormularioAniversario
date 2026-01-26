c6e11c2486e9d3033e44f136c1d23d0b9654c753
Tácito Tinôco Primo <tacito1.filho@gmail.com>
2026-01-26 11:35:11 -0300

    atualização de estilização

--- gerar_qrcode.py
+++ gerar_qrcode.py
@@ -1,6 +1,6 @@
 import qrcode
 
-link_do_site = "https://sofiafaz5.vercel.app/"
+link_do_site = "https://youtube.com/shorts/YMcl2fvSvac?feature=shared"
 
 qr = qrcode.QRCode(
     version=1,
@@ -12,9 +12,9 @@ qr = qrcode.QRCode(
 qr.add_data(link_do_site)
 qr.make(fit=True)
 
-img = qr.make_image(fill_color="#8e44ad", back_color="white")
+img = qr.make_image(back_color="white")
 
 # Salva o arquivo na pasta do seu projeto
-img.save("qrcode_convite.png")
+img.save("tácitoedani.png")
 
-print("✅ QR Code gerado com sucesso como 'qrcode_convite.png'!")
+print("✅ QR Code gerado com sucesso!")
